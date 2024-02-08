e2c3e033ea8d88af6b8533bae161cc586564e053
Matheus <matvno@gmail.com>
2024-02-08 17:54:18 -0300

    feat: ensure user can search for barbershops

--- app/(home)/_components/search.tsx
+++ app/(home)/_components/search.tsx
@@ -3,15 +3,70 @@
 import { Button } from '@/app/_components/ui/button'
 import { Input } from '@/app/_components/ui/input'
 import { SearchIcon } from 'lucide-react'
-import React from 'react'
+import { useRouter } from 'next/navigation'
+import { useForm } from 'react-hook-form'
 
-const Search = () => {
+import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from '@/app/_components/ui/form'
+
+const formSchema = z.object({
+  search: z
+    .string({
+      required_error: 'Campo obrigatório.',
+    })
+    .trim()
+    .min(1, 'Campo obrigatório.'),
+})
+
+interface SearchProps {
+  defaultValues?: z.infer<typeof formSchema>
+}
+
+const Search = ({ defaultValues }: SearchProps) => {
+  const router = useRouter()
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues,
+  })
+
+  const handleSubmit = (data: z.infer<typeof formSchema>) => {
+    router.push(`/barbershops?search=${data.search}`)
+  }
+
   return (
     <div className="flex item-center gap-2">
-      <Input placeholder="Busque por uma barbearia..." />
-      <Button variant="default" size="icon">
-        <SearchIcon size={20} />
-      </Button>
+      <Form {...form}>
+        <form
+          className="flex w-full gap-4"
+          onSubmit={form.handleSubmit(handleSubmit)}
+        >
+          <FormField
+            control={form.control}
+            name="search"
+            render={({ field }) => (
+              <FormItem className="w-full">
+                <FormControl>
+                  <Input placeholder="Busque por uma barbearia..." {...field} />
+                </FormControl>
+
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <Button variant="default" type="submit">
+            <SearchIcon size={20} />
+          </Button>
+        </form>
+      </Form>
     </div>
   )
 }
